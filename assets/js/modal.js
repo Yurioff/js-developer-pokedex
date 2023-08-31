b49ece996fc293b0b8533d3ef7c4de7f2a477360
Yuri <yuri.moreira803@gmail.com>
2023-08-31 16:10:06 -0300

    update modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,11 +1,11 @@
-const modalOpen = document.querySelector('dialog')
-const buttonClose = document.querySelector('dialog button')
+const modalOpen = document.getElementById('janela-modal')
+const buttonClose = document.getElementById('btn-close-modal')
 
 pokemonList.onclick = function () {
     modalOpen.showModal()
 }
 
 buttonClose.onclick = function () {
-    modal.close()
+    modalOpen.close()
 }
 
